522f8b9a432f811fb047ff1ecba82cdaa292b7be
Yulin Zhang <32946065+lannshine@users.noreply.github.com>
2019-05-12 17:00:27 -0700

    Update lab 10 part 2

--- Lab10/Lab10_Part2/Lab10_Part2/main.c
+++ Lab10/Lab10_Part2/Lab10_Part2/main.c
@@ -14,7 +14,6 @@
 #define F_CPU 8000000UL
 #include <util/delay.h>
 
-unsigned char button;
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
@@ -65,43 +64,6 @@ ISR(TIMER1_COMPA_vect) {
 void TimerSet(unsigned long M) {
 	_avr_timer_M = M;
 	_avr_timer_cntcurr = _avr_timer_M;
-}
-
-void set_PWM(double frequency) {
-	static double current_frequency; // Keeps track of the currently set frequency
-	// Will only update the registers when the frequency changes, otherwise allows
-	// music to play uninterrupted.
-	if (frequency != current_frequency) {
-		if (!frequency) { TCCR3B &= 0x08; } //stops timer/counter
-		else { TCCR3B |= 0x03; } // resumes/continues timer/counter
-		
-		// prevents OCR0A from overflowing, using prescaler 64
-		// 0.954 is smallest frequency that will not result in overflow
-		if (frequency < 0.954) { OCR3A = 0xFFFF; }
-		
-		// prevents OCR0A from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
-		else if (frequency > 31250) { OCR3A = 0x0000; }
-		
-		// set OCR0A based on desired frequency
-		else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
-
-		TCNT3 = 0; // resets counter
-		current_frequency = frequency; // Updates the current frequency
-	}
-}
-
-void PWM_on() {
-	TCCR3A = (1 << COM0A0);
-	// COM3A0: Toggle PB3 on compare match between counter and OCR0A
-	TCCR3B = (1 << WGM02) | (1 << CS01) | (1 << CS00);
-	// WGM02: When counter (TCNT0) matches OCR0A, reset counter
-	// CS01 & CS30: Set a prescaler of 64
-	set_PWM(0);
-}
-
-void PWM_off() {
-	TCCR3B = 0x00;
-	TCCR3B = 0x00;
 }
 
 enum TL_States { TL_SMStart, TL_Seq0, TL_Seq1, TL_Seq2 } TL_State;
@@ -165,7 +127,7 @@ void ThreeLED_Tick()
 int main(void)
 {
 	DDRB = 0xFF; PORTB = 0x00; // B output
-	TimerSet(4); //TimerSet(1) or TimerSet(5) do not work
+	TimerSet(5); //TimerSet(1) or TimerSet(5) do not work
                   //TimerSet(10), TimerSet(100) and TimerSet(1000) work
 	TimerOn();
 	BL_State = BL_SMStart;
@@ -176,13 +138,13 @@ int main(void)
 	while(1)
 	{
 		
-		if (tled >= 250)
+		if (tled >= 200)
 		{
 			ThreeLED_Tick();
 			tled = 0;
 		}
 		
-		if (bled >= 250)
+		if (bled >= 200)
 		{
 			BlinkLED_Tick();
 			bled = 0;
